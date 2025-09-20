c7d5ed224f2a22a21dac807d1be77bd98477f012
Juan David Arguello Plata <juandavidarguello@gmail.com>
2025-09-20 16:21:45 -0500

    cd config

--- centralizacion_ci/background/cd-config.sh
+++ centralizacion_ci/background/cd-config.sh
@@ -1,24 +1,8 @@
-#Exposición del Argo Server a través del nginx-ingress
-kubectl apply -f - <<'EOF'
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  name: argocd-server-ingress
-  namespace: argocd
-  annotations:
-    nginx.ingress.kubernetes.io/force-ssl-redirect: "true"
-    nginx.ingress.kubernetes.io/ssl-passthrough: "true"
-spec:
-  ingressClassName: nginx
-  rules:
-  - host: argocd.example.com
-    http:
-      paths:
-      - path: /
-        pathType: Prefix
-        backend:
-          service:
-            name: argocd-server
-            port:
-              name: https
-EOF
+#argo cd
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+#argo cd - cli
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
--- centralizacion_ci/background/installers.sh
+++ centralizacion_ci/background/installers.sh
@@ -25,12 +25,3 @@ helm install external-secrets \
 sudo apt update
 sudo apt install gh
 
-#argo cd
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
-#argo cd - cli
-curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
-sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
-rm argocd-linux-amd64
-
