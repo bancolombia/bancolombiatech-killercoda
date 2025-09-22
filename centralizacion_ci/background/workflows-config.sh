6a7175320e883d9d39048b00eb3459fa598a1727
Juan David Arguello Plata <juandavidarguello@gmail.com>
2025-09-22 08:03:56 -0500

    wait

--- centralizacion_ci/background/workflows-config.sh
+++ centralizacion_ci/background/workflows-config.sh
@@ -3,6 +3,8 @@ kubectl create ns argo-workflows
 kubectl apply -n argo-workflows -f https://github.com/argoproj/argo-workflows/releases/download/v3.7.2/install.yaml
 kubectl -n argo-workflows wait deploy --all --for condition=Available --timeout 2m
 
+kubectl -n argo-workflows wait deploy --all --for condition=Available --timeout 2m
+
 #rbac
 kubectl create serviceaccount argo-workflow -n argo-workflows
 
