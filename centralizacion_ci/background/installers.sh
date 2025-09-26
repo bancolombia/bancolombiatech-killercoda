113a95e55c1e954259a280318a3d8cc9219dba06
Lima Demos Days <juandavidarguello@gmail.com>
2025-09-25 22:01:29 -0500

    MinIO ns

--- centralizacion_ci/background/installers.sh
+++ centralizacion_ci/background/installers.sh
@@ -12,10 +12,10 @@ helm install external-secrets \
     --create-namespace \
 
 #MinIO
-helm install argo-artifacts oci://registry-1.docker.io/bitnamicharts/minio --set fullnameOverride=argo-artifacts
+helm install argo-artifacts oci://registry-1.docker.io/bitnamicharts/minio --set fullnameOverride=argo-artifacts --set namespaceOverride=argo-artifacts
 
-kubectl -n argo-artifacts rollout status --watch --timeout=600s deployment/argo-artifacts-minio-console
-kubectl -n argo-artifacts port-forward --address 0.0.0.0 svc/argo-artifacts-minio-console 9090:9090 > /dev/null &
+kubectl -n argo-artifacts rollout status --watch --timeout=600s deployment/argo-artifacts-console
+kubectl -n argo-artifacts port-forward --address 0.0.0.0 svc/argo-artifacts-console 9090:9090 > /dev/null &
 #helm repo add minio https://charts.min.io/ # official minio Helm charts
 #helm repo update
 #helm install argo-artifacts minio/minio --set service.type=LoadBalancer --set fullnameOverride=argo-artifacts
